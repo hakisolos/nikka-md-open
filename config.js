6801efc164ea1d0121a744193d5aefa214b0ecdd
hakisolos <165590443+hakisolos@users.noreply.github.com>
2024-12-24 14:24:40 +0100

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ module.exports = {
   ANTILINK: toBool(process.env.ANTI_LINK) || false,
   LOGS: toBool(process.env.LOGS) || false,
   ANTILINK_ACTION: process.env.ANTI_LINK || "kick",
-  SESSION_ID:process.env.SESSION_ID || "Nikka-XQ35DEaQB#CAbZtDlOLyjTNk4TVdNpFSTMTwxDD6GBgfMj-uClzMM",
+  SESSION_ID:process.env.SESSION_ID || "Nikka-X16AnVLCT#cd-2khCISq1LAHTRFviY2ytc6jlj9TB95wpmJbVySwk",
   LANG: process.env.LANG || "EN",
   HANDLERS: process.env.PREFIX || '^[!]',
   BRANCH: "main",
